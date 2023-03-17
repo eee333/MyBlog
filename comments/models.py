ec4025a4c532bd49e10a123025f61defba579294
Evgeny Eroshin <eee333@yandex.ru>
2023-03-18 00:12:33 +0300

    Create and setup Posts and Comments app

---
+++ comments/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from posts.models import Post
+from users.models import User
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.author, self.post
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from users.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    foto = models.ImageField(upload_to='posts/', blank=True, verbose_name='Фото')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
