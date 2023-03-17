7b1f02ff2d221959092081a52c75fa42858bba75
Evgeny Eroshin <eee333@yandex.ru>
2023-03-17 16:10:18 +0300

    Create and setup users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from users.managers import CustomUserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.BigIntegerField(null=True, blank=True, verbose_name='Телефон')
+    birthday = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
